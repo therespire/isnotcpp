7af5e465cb2f357659315d94935929607e4d05fe
therespire <r3sp1re666@gmail.com>
2026-06-25 17:24:28 +0300

    what a programming language I am study?

---
+++ main/study.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // this is single-line comment.
+
+    /*
+       and this is multi-line comment.
+       that is another comment, you know?
+    */
+
+    // cout << "Basic" << endl;
+    cout << "C++" << endl;
+    /* cout << "Pascal" << endl;
+    cout << "Python" << endl;
+    cout << "Java Script" << endl; */
+}
