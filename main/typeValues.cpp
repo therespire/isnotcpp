2c61c77ea30c1a88eae9c84030004a8b03c73ef0
therespire <r3sp1re666@gmail.com>
2026-06-25 22:12:38 +0300

    type values in cpp

---
+++ main/typeValues.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// int: integers, no fractional
+int a = 0;
+int b = -12;
+
+// float: integer and fractional parts separated by a symbol "."
+float c = -1.2;
+
+// symbols: '@', type - char
+char d = '%';
+
+int main() {
+  //  cout << 123 << endl;
+  cout << 3.14 << endl;
+  cout << '@' << endl;
+  //  cout << "text" << endl;
+}
