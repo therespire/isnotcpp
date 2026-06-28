96f2ac5bfabd95104810944f9c2249b86a5c7ebf
therespire <r3sp1re666@gmail.com>
2026-06-28 12:28:47 +0300

    update with x in tic tac toe

--- main/output/tictactoe.cpp
+++ main/output/tictactoe.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
   cout << "." << ' ' << "." << ' ' << "." << endl;
+  // cout << "." << ' ' << "x" << ' ' << "." << endl;
   cout << "." << ' ' << "." << ' ' << "." << endl;
   cout << "." << ' ' << "." << ' ' << "." << endl;
 }
