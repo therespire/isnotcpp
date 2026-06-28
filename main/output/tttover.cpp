7809873f2118a0368c08075140d32f182c9b51fd
therespire <r3sp1re666@gmail.com>
2026-06-28 12:37:14 +0300

    game over

---
+++ main/output/tttover.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "o" << ' ' << "." << ' ' << "x" << endl;
+  cout << "o" << ' ' << "x" << ' ' << "x" << endl;
+  cout << "x" << ' ' << "." << ' ' << "o" << endl;
+  cout << endl;
+  cout << "Game over!" << endl;
+}
