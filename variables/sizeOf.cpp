30a64e2752e261b38b52512aa6446364cb8eb695
therespire <r3sp1re666@gmail.com>
2026-07-02 14:59:56 +0300

    sizeof() default function for data types size

---
+++ variables/sizeOf.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "bool " << sizeof(bool) << endl;
+    cout << "char " << sizeof(char) << endl;
+    cout << "int " << sizeof(int) << endl;
+    cout << "float " << sizeof(float) << endl;
+    cout << "double " << sizeof(double) << endl;
+}
