19e95755ea9e8d1184757f5ae5de3372c10a0157
therespire <r3sp1re666@gmail.com>
2026-07-02 14:50:42 +0300

    program for output data types

---
+++ variables/typesOutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "char" << endl;
+    cout << "bool" << endl;
+    cout << "int" << endl;
+    cout << "float" << endl;
+    cout << "double" << endl;
+}
