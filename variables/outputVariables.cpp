9eb7ec6250b711ecf0d2268f2f6964ef5f1a4377
therespire <r3sp1re666@gmail.com>
2026-07-02 17:25:28 +0300

    output a values on variables

---
+++ variables/outputVariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int i = 21;
+    float f = 3.14;
+    string s = "string";
+    
+    cout << i << endl;
+    cout << f << endl;
+    cout << s << endl;
+}
