ac85436213800938a3651af346a96c86105872ab
therespire <r3sp1re666@gmail.com>
2026-07-17 11:50:53 +0300

    enter you name and then I send it to fbi

---
+++ variables/yourName.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // lib for input-output
+#include <string>    // lib for strings
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+}
