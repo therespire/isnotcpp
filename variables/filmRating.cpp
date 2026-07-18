35617cc9f8bc6924dd148e86b5bdef141729bfe5
therespire <r3sp1re666@gmail.com>
2026-07-18 18:11:52 +0300

    rate any film with 5

---
+++ variables/filmRating.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r1, r2, r3, r4, r5 = 0;
+    float f = 0;
+    cin >> r1 >> r2 >> r3 >> r4 >> r5;
+    f += r1 + r2 + r3 + r4 + r5;
+    cout << f / 5;
+}
