404e560c3e98ab1ef98525797e8f2a98a4965a9f
therespire <r3sp1re666@gmail.com>
2026-07-21 18:10:02 +0300

    odd or even

---
+++ logic/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int x;
+    cin >> x;
+    
+    if (x <= 0) {
+        cout << "error";
+    }
+    
+    else {
+        if (x % 2 == 0) {
+            cout << "even";
+        }
+        
+        else {
+            cout << "odd";
+        }
+    }
+}
