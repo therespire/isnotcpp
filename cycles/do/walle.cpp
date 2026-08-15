cf14cc9b33ea0cbf3718a99092a33d6fde1b4f18
therespire <r3sp1re666@gmail.com>
2026-08-15 13:55:22 +0300

    WALL-E

---
+++ cycles/do/walle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+
+    do {
+        cin >> x;
+        cout << x << ' ';
+    }
+    while(x != 0);
+    cout << endl;
+    cout << "WALL-E going to the road...";
+
+}
