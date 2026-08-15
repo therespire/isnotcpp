23c5b471cdf8baddde2b3089f7ab64a2eec50092
therespire <r3sp1re666@gmail.com>
2026-08-15 12:40:10 +0300

    Dobby from Harry Potter

---
+++ cycles/while/dobby.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 1;
+    
+    while (n != 0) {
+        cin >> n;
+        
+        if (n != 0) {
+            cout << "work" << endl;
+        }
+        
+        else {
+            cout << "cool, now I am free bird" << endl;
+            break;
+        }
+    }
+}
